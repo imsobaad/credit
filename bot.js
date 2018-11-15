d6ebd2f8019e84c62ed5ec9b1859a651324d02cc
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-15 18:59:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,6 +68,25 @@ client.on('message', msg => {
 });
 
 
+    client.on('message', message => {
+        var prefix = ".";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
+      });
+
 
 
 
