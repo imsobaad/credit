1dac64e269f75b60159d2e34b06e9fb1521ebddf
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-16 20:58:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,24 +38,7 @@ var channel = "512366415676702724";// ايدي الروم
 });
 
 
-    client.on('message', message => {
-        var prefix = "1";
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
-      
-        let command = message.content.split(" ")[0];
-        command = command.slice(prefix.length);
-      
-      
-      let args = message.content.split(" ").slice(1);
-      let x = args.join(" ")
-        if(message.content.startsWith(prefix + 'say')) {
-            message.channel.send(''+x);
-                message.delete(999)
-        }
-        
-       
-      });
+
 
 
 
