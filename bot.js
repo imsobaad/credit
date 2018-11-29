25ea59ab13f881159010acadcccdab6ed310f0e5
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-29 19:35:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,21 @@ client.on('message', message => {
 
 
 
+client.on('message', async msg => {
+const devs = ['486200045008453635'];
+if(!devs.includes(message.author.id)) return;
+  if(msg.author.bot) return;
+  let prefix = ".";
+  if(!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0].slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+ 
+  if(command == 'say') {
+    if(!args.join(" ")) return msg.delete();
+    msg.channel.send(args.join(" "));
+    return;
+  }
+});
 
 
 
